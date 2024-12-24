91df0a9479431b2489166afdfd650e081e091e47
Cong Luong <conglt@9pay.vn>
2024-12-24 10:51:37 +0700

    update

--- app/View/Components/Auth.php
+++ app/View/Components/Auth.php
@@ -33,7 +33,6 @@ class Auth extends Component
         $languages = language_setting();
 
         $appTheme = $globalSetting;
-        var_dump(session('locale'));die();
         App::setLocale(session('locale') ?? $globalSetting->locale);
 
         return view('components.auth', ['globalSetting' => $globalSetting, 'appTheme' => $appTheme, 'languages' => $languages]);
